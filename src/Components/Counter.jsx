15575f10245095d012e6417b6ea65152c3715521
Devashishsaurabh <onlyvishwasindia@gmail.com>
2022-05-22 13:45:38 +0530

    counter app

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Counter.css'
+const Counter = () => {
+    const [count,setCount]=React.useState(0)
+   const counters=(value)=>{
+       setCount(count+value)
+   }
+   const double=()=>{
+          setCount(count*2)
+   }
+  return (
+    <div>
+    <h1 style={count%2===0? {color:'green'}:{color:'red'}}>Counter App:{count}</h1>
+    <button className='increase' onClick={() =>counters(1)}>Increment</button>
+    <button className="decrese"onClick={() =>counters(-1)}>Decrement</button>
+    <button className="double"onClick={() =>double()}>Double</button>
+    </div>
+  )
+}
+
+export default Counter
